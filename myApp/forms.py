3cbe4d7fe0ce87065569289baff2c19a62d10a0b
saurabh00031 <85858942+saurabh00031@users.noreply.github.com>
2021-10-03 07:00:07 -0700

    forms

--- myApp/forms.py
+++ myApp/forms.py
@@ -5,11 +5,20 @@ from django import forms
 from django.db import transaction
 
 class createUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True,label="Email",widget=forms.EmailInput(attrs={'class':'form-control'}))
+    username=forms.CharField(label='UserName',widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1=forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
     class meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        widgets={
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'password1':forms.PasswordInput(attrs={'class':'form-control'}),
+            'password2':forms.TextInput(attrs={'class':'form-control'}),
+        }
 
     @transaction.atomic
     def save(self):
@@ -20,6 +29,13 @@ class createUserForm(UserCreationForm):
         return user
 
 class UserinfoForm(forms.ModelForm):
+    full_Name=forms.CharField(label='Full_name',widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = Userinfo
         fields = ['full_Name', 'phone', 'state', 'address']
+        widgets={
+            'full_Name':forms.TextInput(attrs={'class':'form-control'}),
+            'phone':forms.TextInput(attrs={'class':'form-control'}),
+            'state':forms.TextInput(attrs={'class':'form-control'}),
+            'address':forms.TextInput(attrs={'class':'form-control'}),
+        }
